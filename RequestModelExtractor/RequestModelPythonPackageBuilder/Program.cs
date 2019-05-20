d7964fb598ed4c35322549d361ccb924b069fc36
Sergei Zubov <szubov@protonmail.com>
2019-05-20 16:58:26 +0300

    Add python request models package builder

---
+++ RequestModelExtractor/RequestModelPythonPackageBuilder/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace RequestModelPythonPackageBuilder
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var modelsDirectory = args[0];
+                var packageName = args[1];
+
+
+                if (!Directory.Exists(modelsDirectory))
+                    throw new ArgumentException("Invalid directory with models: " + modelsDirectory);
+
+                var fileInfos = new DirectoryInfo(modelsDirectory).GetFiles();
+                var imports = new string[fileInfos.Length + 1];
+
+                for (var i = 0; i < fileInfos.Length; i++)
+                {
+                    if (!fileInfos[i].Name.EndsWith("_request.py"))
+                        continue;
+
+                    var pyIndex = fileInfos[i].Name.IndexOf(".py", StringComparison.Ordinal);
+                    var fileName = fileInfos[i].Name
+                        .Substring(0, pyIndex);
+                    var className = string.Join(string.Empty,
+                        fileName.Split('_').Select(s => s.Substring(0, 1).ToUpperInvariant() + s.Substring(1))
+                            .ToArray());
+                    imports[i] = $"from {packageName}.{fileName} import {className}";
+                }
+
+                imports[fileInfos.Length] = Environment.NewLine;
+
+                File.WriteAllText(modelsDirectory + "__init__.py", string.Join(Environment.NewLine, imports));
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid parameters. Please, provide two parameters: path to directory with " +
+                                  "models and package name.");
+                throw;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ RequestModelExtractor/RequestModelPythonPackageBuilder/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RequestModelPythonPackageBuilder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aspose")]
+[assembly: AssemblyProduct("RequestModelPythonPackageBuilder")]
+[assembly: AssemblyCopyright("2019 © Aspose Pty Ltd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("38D4849A-3C85-4C8E-823B-6CD3AEF1AF54")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
