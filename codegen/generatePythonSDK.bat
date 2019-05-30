5ac9b9c7083e2f1502f20d5158765efd1dc64fe8
Sergei Zubov <ssz@li.ru>
2019-05-30 13:13:30 +0300

    Update generatePythonSDK.bat

--- codegen/generatePythonSDK.bat
+++ codegen/generatePythonSDK.bat
@@ -1,5 +1,3 @@
-
-
 set sdkfolder=..\SDKs\Python
 
 copy /y Templates\python\.swagger-codegen-ignore %sdkfolder%\.swagger-codegen-ignore
@@ -9,10 +7,3 @@ java -jar Tools\swagger-codegen-cli-2.3.1.jar generate -i https://api-qa.aspose.
 start /b /wait Tools\RequestModelExtractor.exe %sdkfolder%\asposeimagingcloud\api\ %sdkfolder%\asposeimagingcloud\models\requests\ python || goto :error
 start /b /wait Tools\RequestModelPythonPackageBuilder.exe %sdkfolder%\asposeimagingcloud\models\requests %sdkfolder%\asposeimagingcloud\__init__.py asposeimagingcloud.models.requests || goto :error
 start /b /wait Tools\RequestModelPythonPackageBuilder.exe %sdkfolder%\asposeimagingcloud\models\requests %sdkfolder%\asposeimagingcloud\models\requests\__init__.py asposeimagingcloud.models.requests || goto :error
-
-cd ..
-exit /b 0
-
-:error
-echo Python SDK generation failed
-exit 1
