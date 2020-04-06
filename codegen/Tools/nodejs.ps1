b185a1dd4aa59ffc6059d90143f73a3aebeb40cc
Sergei Zubov <sergei.zubov@aspose.com>
2020-04-06 12:42:50 +0300

    Fix Invoke-WebRequest call

--- codegen/Tools/nodejs.ps1
+++ codegen/Tools/nodejs.ps1
@@ -1,7 +1,7 @@
 $pathToSdk=$args[0]
 
 $lines = Get-Content -encoding UTF8 ..\$pathToSdk\lib\api.ts 
-$jsonSpec = (Invoke-WebRequest https://api-qa.aspose.cloud/v3.0/imaging/swagger/sdkspec | Select-Object -Property Content).content | Out-String | ConvertFrom-Json
+$jsonSpec = (Invoke-WebRequest -UseBasicParsing https://api-qa.aspose.cloud/v3.0/imaging/swagger/sdkspec | Select-Object -Property Content).content | Out-String | ConvertFrom-Json
 
 # Generate Doc
 
